aa1793ffaad933e127ca7c5322c4db0a04e8c0bf
MaiconBortoli <89494572+MaiconBortoli@users.noreply.github.com>
2023-06-12 21:44:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 def eliminacao_gauss(A, b):
     n = len(A)
     
-    # Aplicar eliminação para transformar a matriz A em uma matriz triangular superior
+
     for i in range(n):
-        # Procurar o pivô máximo na coluna atual
-        pivot_row = np.argmax(np.abs(A[i:, i])) + i  # Encontra o índice da linha com o pivô máximo
-        A[[i, pivot_row]] = A[[pivot_row, i]]  # Troca as linhas i e pivot_row
+
+        pivot_row = np.argmax(np.abs(A[i:, i])) + i 
+        A[[i, pivot_row]] = A[[pivot_row, i]] 
         b[[i, pivot_row]] = b[[pivot_row, i]]
         
         # Fazer a eliminação
@@ -16,14 +16,14 @@ def eliminacao_gauss(A, b):
             A[j, i:] -= factor * A[i, i:]
             b[j] -= factor * b[i]
     
-    # Resolver o sistema triangular superior
+  
     x = np.zeros(n)
     for i in range(n - 1, -1, -1):
         x[i] = (b[i] - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
     
     return x
 
-# Exemplo de uso
+# MATRIZ
 A = np.array([[1, 6, 2, 4],
               [3, 19, 4, 15],
               [1, 4, 8, -12],
